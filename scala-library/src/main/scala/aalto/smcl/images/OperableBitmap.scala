762f1c3a351cb0144767bc200da38292d8acf7b1
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2015-07-13 15:44:50 +0300

    Corrected a comment

--- scala-library/src/main/scala/aalto/smcl/images/OperableBitmap.scala
+++ scala-library/src/main/scala/aalto/smcl/images/OperableBitmap.scala
@@ -4,7 +4,7 @@ import java.awt.{ Graphics2D => JGraphics2D }
 import aalto.smcl.images.operations.BitmapOperation
 
 /**
- * Ensures that the Java's `Graphics2D` API is available for use.
+ * Ensures that a bitmap can be operated with using [[BitmapOperation]] instances.
  *
  * @author Aleksi Lukkarinen
  */
