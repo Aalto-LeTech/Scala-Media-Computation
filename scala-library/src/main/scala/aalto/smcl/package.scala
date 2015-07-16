abab5de1722ab125946f40b98d2fcc1b80a6a785
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2015-07-16 08:56:41 +0300

    Relocated some functionality, Part 2

--- scala-library/src/main/scala/aalto/smcl/package.scala
+++ scala-library/src/main/scala/aalto/smcl/package.scala
@@ -6,33 +6,4 @@ package aalto
  *
  * @author Aleksi Lukkarinen
  */
-package object smcl {
-
-  /** An empty string. */
-  protected[smcl] val STR_EMPTY = ""
-
-  /** A string containing a single space. */
-  protected[smcl] val STR_SPACE = " "
-
-  /** A string containing a single colon. */
-  protected[smcl] val STR_COLON = ":"
-
-  /** A string containing a single colon. */
-  protected[smcl] val STR_COLON_AS_UNICODE = "\u003A"
-
-  /** A string containing a single semicolon. */
-  protected[smcl] val STR_SEMICOLON = ";"
-
-  /** A string containing a single semicolon. */
-  protected[smcl] val STR_SEMICOLON_AS_UNICODE = "\u003B"
-
-  /** A string containing a single left angle bracket. */
-  protected[smcl] val STR_LEFT_ANGLE_BRACKET = "["
-
-  /** A string containing a single right angle bracket. */
-  protected[smcl] val STR_RIGHT_ANGLE_BRACKET = "]"
-
-  /** A string containing a single zero. */
-  protected[smcl] val STR_ZERO = "0"
-
-}
+package object smcl {}
