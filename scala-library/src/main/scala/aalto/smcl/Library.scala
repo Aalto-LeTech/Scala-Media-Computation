cb9b28436787040848cb3b56a0a59b76206b940d
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2015-07-27 01:23:08 +0300

    Introduced Library object

---
+++ scala-library/src/main/scala/aalto/smcl/Library.scala
@@ -0,0 +1,29 @@
+package aalto.smcl
+
+
+/**
+ * Information and functionality concerning the SMCL as a whole.
+ *
+ * @author Aleksi Lukkarinen
+ */
+object Library {
+
+  /** Full name of this library. */
+  val FULL_NAME: String = "Scala Media Computation Library"
+
+  /** Abbreviated name of this library. */
+  val ABBREVIATED_NAME: String = "SMCL"
+
+  /** Major part of the version identifier of this library. */
+  val VERSION_MAJOR: Int = 0
+
+  /** Minor part of the version identifier of this library. */
+  val VERSION_MINOR: Int = 1
+
+  /** Full version identifier of this library. */
+  val VERSION_STRING: String = s"$VERSION_MAJOR.$VERSION_MINOR"
+
+  /** Name of this library's original author. */
+  val ORIGINAL_AUTHOR_NAME: String = "Aleksi Lukkarinen"
+
+}
