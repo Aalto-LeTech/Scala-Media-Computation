966c61d48e60528194cb0f385d3ceb0ea18d39f1
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2015-08-02 12:52:36 +0300

    Added a class for creating arrays of circles

---
+++ scala-library/src/main/scala/aalto/smcl/images/immutable/collections/CirclesA.scala
@@ -0,0 +1,47 @@
+package aalto.smcl.images.immutable.collections
+
+import aalto.smcl.images.immutable.primitives.{Bitmap, Circle}
+
+import aalto.smcl.common.{Color, GS}
+import aalto.smcl.images.SettingKeys.{DefaultBackground, DefaultBitmapWidthInPixels, DefaultPrimary}
+import aalto.smcl.images.immutable.primitives.Bitmap.ViewerUpdateStyle.PreventViewerUpdates
+
+import scala.collection.mutable
+
+
+/**
+ *
+ *
+ * @author Aleksi Lukkarinen
+ */
+object CirclesA {
+
+    aalto.smcl.images.SettingsInitializer.perform()
+
+    /**
+     * Creates an array of [[Bitmap]] instances with a circle drawn on each bitmap.
+     *
+     * @param diameter
+     * @param color
+     * @param backgroundColor
+     * @return
+     */
+    def apply(
+      collectionSize: Int = 5,
+      diameter: Int = GS.intFor(DefaultBitmapWidthInPixels),
+      color: Color = GS.colorFor(DefaultPrimary),
+      backgroundColor: Color = GS.colorFor(DefaultBackground)): Array[Bitmap] = {
+
+      require(collectionSize >= 0, s"Size of the collection cannot be negative (was $collectionSize)")
+
+      val newCollection = mutable.ArrayBuffer.empty[Bitmap]
+
+      var item = 0
+      for (item <- 1 to collectionSize) {
+        newCollection += Circle(diameter, color, backgroundColor, PreventViewerUpdates)
+      }
+
+      newCollection.toArray
+   }
+
+ }
