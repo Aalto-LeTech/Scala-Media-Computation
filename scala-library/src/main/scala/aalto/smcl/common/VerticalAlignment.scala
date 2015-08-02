b8b08ad99b3a0f8628a2bd0006d60bd294af3589
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2015-08-03 02:55:06 +0300

    Added enums for horizontal and vertical alignments

---
+++ scala-library/src/main/scala/aalto/smcl/common/VerticalAlignment.scala
@@ -0,0 +1,22 @@
+package aalto.smcl.common
+
+/**
+ * Vertical alignment positions.
+ *
+ * @author Aleksi Lukkarinen
+ */
+object VerticalAlignment extends Enumeration {
+
+  /** Type alias for this enumeration. */
+  type VerticalAlignment = Value
+
+  /** Align to the top. */
+  val Top = Value
+
+  /** Align to the middle. */
+  val Middle = Value
+
+  /** Align to the bottom. */
+  val Bottom = Value
+
+ }
