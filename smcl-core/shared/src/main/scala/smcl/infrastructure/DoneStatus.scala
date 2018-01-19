4b41380d6f8d0b7de5dc84d0f426be2b3f3f21fe
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2018-01-19 18:14:29 +0200

    Adds DoneStatus class

---
+++ smcl-core/shared/src/main/scala/smcl/infrastructure/DoneStatus.scala
@@ -0,0 +1,51 @@
+/* .            .           .                   .                 +             .          +      */
+/*         +-----------+  +---+    +  +---+  +-----------+  +---+    Media Programming in Scala   */
+/*   *     |           |  |    \     /    |  |           | +|   |            Since 2015           */
+/*         |   +-------+  |     \   /     |  |   +-------+  |   |   .                        .    */
+/*         |   |          |      \ /      |  |   |          |   |         Aalto University        */
+/*       . |   +-------+  |   .   V   .   |  |   |   .      |   |      .   Espoo, Finland       . */
+/*  +      |           |  |   |\     /|   |  |   |          |   |                  .    +         */
+/*         +------+    |  |   | \   / |   |  |   |          |   |    +        *                   */
+/*    *           |    |  |   |  \ /  |   |  |   |      *   |   |                     .      +    */
+/*      -- +------+    |  |   |   V  *|   |  |   +-------+  |   +-------+ --    .                 */
+/*    ---  |           |  |   | .     |   |  |           |  |           |  ---      +      *      */
+/*  ------ +-----------+  +---+       +---+  +-----------+  +-----------+ ------               .  */
+/*                                                                                     .          */
+/*     T H E   S C A L A   M E D I A   C O M P U T A T I O N   L I B R A R Y      .         +     */
+/*                                                                                    *           */
+
+package smcl.infrastructure
+
+
+/**
+ *
+ *
+ * @author Aleksi Lukkarinen
+ */
+class DoneStatus {
+
+  /** */
+  private
+  var hasNotBeenDone = true
+
+  /**
+   *
+   */
+  def setDone(): Unit = {
+    if (hasNotBeenDone) {
+      synchronized{
+        if (hasNotBeenDone) {
+          hasNotBeenDone = false
+        }
+      }
+    }
+  }
+
+  /**
+   *
+   *
+   * @return
+   */
+  def isNotDone: Boolean = hasNotBeenDone
+
+}
