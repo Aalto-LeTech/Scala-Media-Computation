854fba8a987eb5fb85eaa1b4e33089a7e75e9290
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2018-04-20 21:17:24 +0300

    Starts to implement scaling for Arc

--- smcl-core/shared/src/main/scala/smcl/pictures/Arc.scala
+++ smcl-core/shared/src/main/scala/smcl/pictures/Arc.scala
@@ -363,31 +363,6 @@ class Arc private(
   }
 
   /**
-   *
-   *
-   * @param widthFactor
-   * @param heightFactor
-   *
-   * @return
-   */
-  override
-  def scaleByRelativeToOrigo(
-      widthFactor: Double,
-      heightFactor: Double): Arc = {
-
-    val newWidthFactor = widthFactor * currentHorizontalScalingFactor
-    val newHeightFactor = heightFactor * currentVerticalScalingFactor
-    val newTransformation =
-      currentTransformation.scaleRelativeToPoint(
-        newWidthFactor, newHeightFactor, position)
-
-    internalCopy(
-      newHorizontalScalingFactor = newWidthFactor,
-      newVerticalScalingFactor = newHeightFactor,
-      newTransformation = newTransformation)
-  }
-
-  /**
    * Rotates this object around origo (0,0) by 90 degrees clockwise.
    *
    * @return
@@ -590,6 +565,388 @@ class Arc private(
 
     internalCopy(
       newRotationAngleInDegrees = newRotationAngle,
+      newTransformation = newTransformation)
+  }
+
+  /**
+   * Scales this object to a given width in relation to its center.
+   *
+   * @param targetWidth
+   *
+   * @return
+   */
+  override
+  def scaleHorizontallyTo(targetWidth: Double): Arc =
+    scaleHorizontallyTo(targetWidth, position)
+
+  /**
+   * Scales this object to a given width in relation to a given point.
+   *
+   * @param targetWidth
+   * @param relativityPoint
+   *
+   * @return
+   */
+  override
+  def scaleHorizontallyTo(
+      targetWidth: Double,
+      relativityPoint: Pos): Arc = {
+
+    scaleTo(
+      targetWidth,
+      targetHeight = height.inPixels,
+      relativityPoint = relativityPoint)
+  }
+
+  /**
+   * Scales this object to a given width in relation to the origo.
+   *
+   * @param targetWidth
+   *
+   * @return
+   */
+  override
+  def scaleHorizontallyToRelativeToOrigo(targetWidth: Double): Arc =
+    scaleToRelativeToOrigo(
+      targetWidth,
+      targetHeight = height.inPixels)
+
+  /**
+   * Scales this object to a given height in relation to its center.
+   *
+   * @param targetHeight
+   *
+   * @return
+   */
+  override
+  def scaleVerticallyTo(targetHeight: Double): Arc =
+    scaleVerticallyTo(targetHeight, position)
+
+  /**
+   * Scales this object to a given height in relation to a given point.
+   *
+   * @param targetHeight
+   * @param relativityPoint
+   *
+   * @return
+   */
+  override
+  def scaleVerticallyTo(
+      targetHeight: Double,
+      relativityPoint: Pos): Arc = {
+
+    scaleTo(
+      targetWidth = width.inPixels,
+      targetHeight = targetHeight,
+      relativityPoint = relativityPoint)
+  }
+
+  /**
+   * Scales this object to a given height in relation to the origo.
+   *
+   * @param targetHeight
+   *
+   * @return
+   */
+  override
+  def scaleVerticallyToRelativeToOrigo(targetHeight: Double): Arc =
+    scaleToRelativeToOrigo(
+      targetWidth = width.inPixels,
+      targetHeight = targetHeight)
+
+  /**
+   * Scales this object in relation to its center by
+   * using a single length for both width and height.
+   *
+   * @param targetSideLength
+   *
+   * @return
+   */
+  override
+  def scaleTo(targetSideLength: Double): Arc =
+    scaleTo(targetSideLength, position)
+
+  /**
+   * Scales this object in relation to a given point by
+   * using a single length for both width and height.
+   *
+   * @param targetSideLength
+   * @param relativityPoint
+   *
+   * @return
+   */
+  override
+  def scaleTo(
+      targetSideLength: Double,
+      relativityPoint: Pos): Arc = {
+
+    scaleTo(
+      targetWidth = targetSideLength,
+      targetHeight = targetSideLength,
+      relativityPoint = relativityPoint)
+  }
+
+  /**
+   * Scales this object in relation to the origo by
+   * using a single length for both width and height.
+   *
+   * @param targetSideLength
+   *
+   * @return
+   */
+  override
+  def scaleToRelativeToOrigo(targetSideLength: Double): Arc =
+    scaleToRelativeToOrigo(
+      targetWidth = targetSideLength,
+      targetHeight = targetSideLength)
+
+  /**
+   * Scales this object to given width and height in relation to its center.
+   *
+   * @param targetWidth
+   * @param targetHeight
+   *
+   * @return
+   */
+  override
+  def scaleTo(
+      targetWidth: Double,
+      targetHeight: Double): Arc = {
+
+    scaleTo(targetWidth, targetHeight, position)
+  }
+
+  /**
+   * Scales this object to given width and height in relation to a given point.
+   *
+   * @param targetWidth
+   * @param targetHeight
+   * @param relativityPoint
+   *
+   * @return
+   */
+  override
+  def scaleTo(
+      targetWidth: Double,
+      targetHeight: Double,
+      relativityPoint: Pos): Arc = {
+
+    val (horizontalFactor, verticalFactor) =
+      scalingFactorsFor(targetWidth, targetHeight)
+
+    copy(newPoints = points.map(_.scaleBy(horizontalFactor, verticalFactor, relativityPoint)))
+  }
+
+  /**
+   * Scales this object to given width and height in relation to the origo.
+   *
+   * @param targetWidth
+   * @param targetHeight
+   *
+   * @return
+   */
+  override
+  def scaleToRelativeToOrigo(
+      targetWidth: Double,
+      targetHeight: Double): Arc = {
+
+    val (horizontalFactor, verticalFactor) =
+      scalingFactorsFor(targetWidth, targetHeight)
+
+    copy(newPoints = points.map(_.scaleByRelativeToOrigo(horizontalFactor, verticalFactor)))
+  }
+
+  /**
+   *
+   *
+   * @param targetWidth
+   * @param targetHeight
+   *
+   * @return
+   */
+  def scalingFactorsFor(
+      targetWidth: Double,
+      targetHeight: Double): (Double, Double) = {
+
+    val horizontalFactor = targetWidth / width.inPixels
+    val verticalFactor = targetHeight / height.inPixels
+
+    (horizontalFactor, verticalFactor)
+  }
+
+  /**
+   * Scales this object horizontally in relation to its center.
+   *
+   * @param factor
+   *
+   * @return
+   */
+  override
+  def scaleHorizontallyBy(factor: Double): Arc =
+    copy(newPoints = points.map(_.scaleHorizontallyBy(factor)))
+
+  /**
+   * Scales this object horizontally in relation to a given point.
+   *
+   * @param factor
+   * @param relativityPoint
+   *
+   * @return
+   */
+  override
+  def scaleHorizontallyBy(
+      factor: Double,
+      relativityPoint: Pos): Arc = {
+
+    copy(newPoints = points.map(_.scaleHorizontallyBy(factor, relativityPoint)))
+  }
+
+  /**
+   * Scales this object horizontally in relation to the origo.
+   *
+   * @param factor
+   *
+   * @return
+   */
+  override
+  def scaleHorizontallyByRelativeToOrigo(factor: Double): Arc =
+    copy(newPoints = points.map(_.scaleHorizontallyByRelativeToOrigo(factor)))
+
+  /**
+   * Scales this object vertically in relation to its center.
+   *
+   * @param factor
+   *
+   * @return
+   */
+  override
+  def scaleVerticallyBy(factor: Double): Arc =
+    copy(newPoints = points.map(_.scaleVerticallyBy(factor)))
+
+  /**
+   * Scales this object vertically in relation to a given point.
+   *
+   * @param factor
+   * @param relativityPoint
+   *
+   * @return
+   */
+  override
+  def scaleVerticallyBy(
+      factor: Double,
+      relativityPoint: Pos): Arc = {
+
+    copy(newPoints = points.map(_.scaleBy(factor, relativityPoint)))
+  }
+
+  /**
+   * Scales this object vertically in relation to the origo.
+   *
+   * @param factor
+   *
+   * @return
+   */
+  override
+  def scaleVerticallyByRelativeToOrigo(factor: Double): Arc =
+    copy(newPoints = points.map(_.scaleVerticallyByRelativeToOrigo(factor)))
+
+  /**
+   * Scales this object in relation to its center by using a given factor
+   * for both horizontal and vertical directions.
+   *
+   * @param factor
+   *
+   * @return
+   */
+  override
+  def scaleBy(factor: Double): Arc =
+    copy(newPoints = points.map(_.scaleBy(factor)))
+
+  /**
+   * Scales this object in relation to a given point by using a given factor
+   * for both horizontal and vertical directions.
+   *
+   * @param factor
+   * @param relativityPoint
+   *
+   * @return
+   */
+  override
+  def scaleBy(
+      factor: Double,
+      relativityPoint: Pos): Arc = {
+
+    copy(newPoints = points.map(_.scaleBy(factor, relativityPoint)))
+  }
+
+  /**
+   * Scales this object in relation to the origo by using a given factor for
+   * both horizontal and vertical directions.
+   *
+   * @param factor
+   *
+   * @return
+   */
+  override
+  def scaleByRelativeToOrigo(factor: Double): Arc =
+    copy(newPoints = points.map(_.scaleByRelativeToOrigo(factor)))
+
+  /**
+   * Scales this object by given horizontal and vertical factors in relation to its center.
+   *
+   * @param horizontalFactor
+   * @param verticalFactor
+   *
+   * @return
+   */
+  override
+  def scaleBy(
+      horizontalFactor: Double,
+      verticalFactor: Double): Arc = {
+
+    copy(newPoints = points.map(_.scaleBy(horizontalFactor, verticalFactor)))
+  }
+
+  /**
+   * Scales this object by given horizontal and vertical factors in relation to a given point.
+   *
+   * @param horizontalFactor
+   * @param verticalFactor
+   * @param relativityPoint
+   *
+   * @return
+   */
+  override
+  def scaleBy(
+      horizontalFactor: Double,
+      verticalFactor: Double,
+      relativityPoint: Pos): Arc = {
+
+    copy(newPoints = points.map(_.scaleBy(horizontalFactor, verticalFactor, relativityPoint)))
+  }
+
+  /**
+   * Scales this object by given horizontal and vertical factors in relation to the origo.
+   *
+   * @param horizontalFactor
+   * @param verticalFactor
+   *
+   * @return
+   */
+  override
+  def scaleByRelativeToOrigo(
+      horizontalFactor: Double,
+      verticalFactor: Double): Arc = {
+
+    val newWidthFactor = horizontalFactor * currentHorizontalScalingFactor
+    val newHeightFactor = verticalFactor * currentVerticalScalingFactor
+    val newTransformation =
+      currentTransformation.scaleRelativeToPoint(
+        newWidthFactor, newHeightFactor, position)
+
+    internalCopy(
+      newHorizontalScalingFactor = newWidthFactor,
+      newVerticalScalingFactor = newHeightFactor,
       newTransformation = newTransformation)
   }
 
